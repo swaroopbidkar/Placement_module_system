a6fa09883b0c0ca4273a9385181aa6af7aa232c1
Swaroopbidkar <swaroopbidkar@gmail.com>
2024-07-26 03:30:17 +0530

    Save local changes before revert

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.use('/student', require('./student'));
-router.use('/teacher', require('./teacher'));
-router.use('/company', require('./company'));
-
-module.exports = router;
