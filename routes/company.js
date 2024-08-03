8752bd1e993e91dc30f06f26d4325b2f0a23face
Swaroopbidkar <swaroopbidkar@gmail.com>
2024-08-03 23:18:08 +0530

    ADMIN CHANGE

---
+++ routes/company.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const session = require('express-session');
+const router = express.Router();
+const Company = require('../models/company');
+const Student = require('../models/student');
+
+router.use(session({
+    secret: 'secret', 
+    resave: true,
+    saveUninitialized: true
+}));
+
+router.get('/login', (req, res) => {
+    res.render('company_login');
+});
+
+
+router.get('/register', (req, res) => {
+    res.render('company_register');
+});
+
+
+module.exports = router;
